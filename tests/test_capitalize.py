ba9cc02794dcd3bc999f23da57c8ddac6b4b9640
nerooren <nerooren4001@gmail.com>
2022-07-16 20:51:08 +0300

    Independent work#1 added

---
+++ tests/test_capitalize.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+from package-name.capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
